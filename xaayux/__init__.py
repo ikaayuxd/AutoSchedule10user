7b812f7e1334d0cdea4c40dbdf856e358fb2633f
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-02-05 22:49:50 +0530

    Update __init__.py

--- xaayux/__init__.py
+++ xaayux/__init__.py
@@ -3,15 +3,10 @@ from os import environ
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+from config import API_ID, API_HASH, SESSION, CHANNEL_ID, TIME
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
-
-API_ID = int(environ.get("API_ID"))
-API_HASH = environ.get("API_HASH")
-SESSION = environ.get("SESSION")
-TIME = int(environ.get("TIME", "60"))
-CHANNEL_ID = int(environ.get("CHANNEL_ID", "-1001815790599"))
 
 
 if SESSION is not None:
