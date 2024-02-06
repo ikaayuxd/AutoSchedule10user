04e2114d6962c67d897a5d6100ea70f4a6ffb795
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-02-06 15:12:31 +0530

    Update Auto.py

--- xaayux/plugins/Auto.py
+++ xaayux/plugins/Auto.py
@@ -3,31 +3,29 @@ from telethon import events, types
 import logging 
 import asyncio
 import random
-from telethon.tl.types import Channel
 
-folder_name = "ProHacking"  # Replace with the name of your fo
+channel_ids = [-1002095710352, -1002052982599, -1002048287965, -1001998377282, -1001992984787, -1001624878815, -1001943016652, -1002139659491]  # Replace with your channel IDs
+
 messages = [
     f"𝗛𝗔𝗖𝗞 𝗬𝗢𝗨𝗥 𝗦𝗖𝗛𝗢𝗢𝗜/𝗖𝗢𝗜𝗜𝗘𝗚𝗘 𝗪𝗘𝗕𝗦𝗜𝗧𝗘 ⬇️\n\n𝗜𝗜𝗡𝗞: [@WebHacking56](https://t.me/+hDX1CM9vk5U5MDI9)\n\n𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗙𝗢𝗜𝗗𝗘𝗥 𝗘𝗡𝗧𝗥𝗬 ⬇️\n\n𝗜𝗜𝗡𝗞: [@PaidFolder68](https://t.me/addlist/3pwjeI2RyiMzYWE9)", 
     f"𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗖𝗛𝗔𝗡𝗡𝗘𝗜 𝗘𝗡𝗧𝗥𝗬 ⬇️\n\n𝗜𝗜𝗡𝗞: [@LegendxTricks](https://t.me/+98qlvU9in_xlODZl)\n\n𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗙𝗢𝗜𝗗𝗘𝗥 𝗘𝗡𝗧𝗥𝗬 ⬇️\n\n𝗜𝗜𝗡𝗞: [@PaidFolder68](https://t.me/addlist/3pwjeI2RyiMzYWE9)"
 ]  # Add your desired message links here
 
-async def forward_messages():
+async def send_messages():
     while True:
-        for message in messages:
+        for channel_id in channel_ids:
             message = random.choice(messages)
-            async for dialog in client.iter_dialogs():
-                if dialog.is_channel and dialog.folder_id == folder_name:
-                    await client.send_message(dialog.id, message)
-        await asyncio.sleep(5)  # Send a message every 5 seconds
+            await client.send_message(channel_id, message)
+        await asyncio.sleep(10)  # Send a message every 1 minute
 
 @client.on(events.NewMessage(outgoing=True, pattern='!cancel'))
 async def handle_cancel(event):
     await event.respond('Cancelling Auto Message Forwarding...')
-    global forward_task
-    forward_task.cancel()
+    global send_task
+    send_task.cancel()
 
 @client.on(events.NewMessage(outgoing=True, pattern='!start'))
 async def handle_start(event):
     await event.respond("Starting Auto Message Forwarding...")
-    global forward_task
-    forward_task = asyncio.create_task(forward_messages())
+    global send_task
+    send_task = asyncio.create_task(send_messages())
