8f942d30295dfff4132bb0621a840a0f8489f827
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-02-06 09:08:24 +0530

    Update Auto.py

--- xaayux/plugins/Auto.py
+++ xaayux/plugins/Auto.py
@@ -5,15 +5,15 @@ import asyncio
 import random
 
 folder_name = "4"  # Replace with the name of your fo
-message = [
+messages = [
     f"𝗛𝗔𝗖𝗞 𝗬𝗢𝗨𝗥 𝗦𝗖𝗛𝗢𝗢𝗜/𝗖𝗢𝗜𝗜𝗘𝗚𝗘 𝗪𝗘𝗕𝗦𝗜𝗧𝗘 ⬇️\n\n𝗜𝗜𝗡𝗞: [@WebHacking56](https://t.me/+hDX1CM9vk5U5MDI9)\n\n𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗙𝗢𝗜𝗗𝗘𝗥 𝗘𝗡𝗧𝗥𝗬 ⬇️\n\n𝗜𝗜𝗡𝗞: [@PaidFolder68](https://t.me/addlist/3pwjeI2RyiMzYWE9)", 
     f"𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗖𝗛𝗔𝗡𝗡𝗘𝗜 𝗘𝗡𝗧𝗥𝗬 ⬇️\n\n𝗜𝗜𝗡𝗞: [@LegendxTricks](https://t.me/+98qlvU9in_xlODZl)\n\n𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗙𝗢𝗜𝗗𝗘𝗥 𝗘𝗡𝗧𝗥𝗬 ⬇️\n\n𝗜𝗜𝗡𝗞: [@PaidFolder68](https://t.me/addlist/3pwjeI2RyiMzYWE9)"
 ]  # Add your desired message links here
 
 async def forward_messages():
     while True:
-        for message_link in message_links:
-            message = random.choice(message)
+        for message in messages:
+            message = random.choice(messages)
             for dialog in await client.get_dialogs():
                 if isinstance(dialog.entity, Channel) and dialog.entity.megagroup and dialog.entity.folder_id == folder_name:
                     await client.send_message(dialog.entity.id, message)
