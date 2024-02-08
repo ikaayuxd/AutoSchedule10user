2ea4dd81827766df6183b32e49d90d3df59646a6
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-02-08 08:47:03 +0530

    Update Auto.py

--- xaayux/plugins/Auto.py
+++ xaayux/plugins/Auto.py
@@ -3,13 +3,7 @@ from telethon import events, types
 import logging 
 import asyncio
 import random
-
-channel_ids = [-1002031399140, -1002095710352, -1002052982599, -1002048287965, -1001998377282, -1001992984787, -1001624878815, -1001943016652, -1002139659491]  # Replace with your channel IDs
-
-messages = [
-    f"𝗛𝗔𝗖𝗞 𝗬𝗢𝗨𝗥 𝗦𝗖𝗛𝗢𝗢𝗜/𝗖𝗢𝗜𝗜𝗘𝗚𝗘 𝗪𝗘𝗕𝗦𝗜𝗧𝗘 ⬇️\n\n𝗜𝗜𝗡𝗞: [@WebHacking56](https://t.me/+hDX1CM9vk5U5MDI9)\n\n𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗙𝗢𝗜𝗗𝗘𝗥 𝗘𝗡𝗧𝗥𝗬 ⬇️\n\n𝗜𝗜𝗡𝗞: [@PaidFolder68](https://t.me/addlist/3pwjeI2RyiMzYWE9)\n\n𝗔𝗗𝗗 𝗬𝗢𝗨𝗥: @AnOnYmOuS_DxT", 
-    f"𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗖𝗛𝗔𝗡𝗡𝗘𝗜 𝗘𝗡𝗧𝗥𝗬 ⬇️\n\n𝗜𝗜𝗡𝗞: [@LegendxTricks](https://t.me/+98qlvU9in_xlODZl)\n\n𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗙𝗢𝗜𝗗𝗘𝗥 𝗘𝗡𝗧𝗥𝗬 ⬇️\n\n𝗜𝗜𝗡𝗞: [@PaidFolder68](https://t.me/addlist/3pwjeI2RyiMzYWE9)\n\n𝗔𝗗𝗗 𝗬𝗢𝗨𝗥: @AnOnYmOuS_DxT"
-]  # Add your desired message links here
+from xaayux.config import channel_ids, messages
 
 async def send_messages():
     while True:
