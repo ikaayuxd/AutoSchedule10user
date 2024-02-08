8f0b3cefefe19b4efe245d3a55b9475f5b7d33ff
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-02-08 08:58:37 +0530

    Update ping.py

--- xaayux/plugins/ping.py
+++ xaayux/plugins/ping.py
@@ -1,4 +1,4 @@
-#© 𝙄𝙩𝙨 ⚡ 𝙅𝙤𝙚𝙡 | #𝘼𝙗𝙊𝙪𝙩𝙈𝙚_𝘿𝙆
+
 
 from .. import client
 from telethon import events
@@ -11,32 +11,20 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 # -- Constants -- #
-HELP = """
-𝘼𝙪𝙩𝙤𝙋𝙞𝙘𝙓 __Commands__
-
-`!start` - __To Start Changing DP__
-`!cancel` - __To Cancel Changing DP__
-`!alive` - __To Check If Bot Is Alive__
-`!repo` - __To Get The Repo__
-`!about` - __Details About Me__
-`!help` - __For This Message__
+HELP = """𝗔𝘂𝘁𝗼 𝗦𝗰𝗵𝗲𝗱𝘂𝗹𝗲𝗿 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀
+`!start` - **Start Auto Scheduler **
+`!cancel` - **Stop Auto Scheduler** 
+`!alive` - **Check If Bot Is Alive**
+`!about` - **About The Bot **
+`!help` - **Help Message**
 """
 
 ABOUT_TXT = """
-᪥ **Name:** 𝘼𝙪𝙩𝙤𝙋𝙞𝙘𝙓
+᪥ **Name:** 𝗔𝘂𝘁𝗼 𝗦𝗰𝗵𝗲𝗱𝘂𝗹𝗲𝗿 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 𝗕𝘆 @xAaYux • @LegendxTricks
 ᪥ **Library: [Telethon](https://docs.telethon.dev/)**
 ᪥ **Language: [Python 3](https://www.python.org)**
-᪥ **Dev:** [𝙄𝙩𝙨 ⚡ 𝙅𝙤𝙚𝙡](https://t.me/joel_noob)
-᪥ **Inspiration: [Dᴋ 🇮🇳](https://t.me/AbOutMe_DK)**
+᪥ **Dev:** [⏤‌ＫＡＲＴＩＫ𓆩♡𓆪™|🇮🇳](https://t.me/xAaYux)
 """
-
-REPO = """
-𝘼𝙪𝙩𝙤𝙋𝙞𝙘𝙓 __is an Open Source UserBot based on Telethon you can access it's source code from **[here](https://github.com/git-itsjoel/AutoPicX)**__
-"""
-
-@client.on(events.NewMessage(outgoing=True, pattern='!repo'))
-async def repo(event):
-    await event.edit(REPO, link_preview=False)
 
 @client.on(events.NewMessage(outgoing=True, pattern='!about'))
 async def about(event):
@@ -51,7 +39,10 @@ async def help_me(event):
 @client.on(events.NewMessage(outgoing=True, pattern='!alive'))
 async def alive(event):
     txt = await event.edit("▢▢▢")
-    await event.edit("▣▢▢")
-    await event.edit("▣▣▢")
-    await event.edit("▣▣▣")
-    await event.edit("𝘼𝙪𝙩𝙤𝙋𝙞𝙘𝙓 𝙄𝙨 𝘼𝙘𝙩𝙞𝙫𝙚")
+    await event.edit("▣▢▢▢▢▢")
+    await event.edit("▣▣▢▢▢▢")
+    await event.edit("▣▣▣▢▢▢")
+    await event.edit("▣▣▣▣▢▢")
+    await event.edit("▣▣▣▣▣▢")
+    await event.edit("▣▣▣▣▣▣")
+    await event.edit("𝗔𝘂𝘁𝗼 𝗦𝗰𝗵𝗲𝗱𝘂𝗹𝗲𝗿 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 𝗜𝘀 𝗔𝗰𝘁𝗶𝘃𝗲 𝗔𝗻𝗱 𝗦𝘁𝗮𝗿𝘁𝗲𝗱 𝗦𝗲𝗻𝗱𝗶𝗻𝗴 𝗔𝗻𝗱 𝗦𝗰𝗵𝗲𝗱𝘂𝗹𝗶𝗻𝗴 𝗗𝗲𝗮𝗹𝘆 𝗜𝘀 𝗦𝗲𝘁 𝗧𝗼 {DELAY}(𝗦𝗲𝗰𝗼𝗻𝗱)")
