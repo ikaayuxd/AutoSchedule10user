ac1712d183448d17f8a54f50810ddcfbe43f6cd7
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-03-17 23:36:21 +0530

    Update Auto.py

--- xaayux/plugins/Auto.py
+++ xaayux/plugins/Auto.py
@@ -1,41 +1,29 @@
 from .. import client, DELAY
-from telethon.tl.functions.messages import SendMessageRequest
-from telethon.tl.types import InputPeerChannel
 from telethon import events, types
 import logging 
 import asyncio
 import random
-from xaayux.config import channel_ids, messages, DELAY, group_ids
+from xaayux.config import channel_ids, messages, DELAY
 
-groups = 'groups.txt'
-with open(groups, 'r') as file:
-        group_ids = [line.strip() for line in file]
-    
 async def send_messages():
     while True:
-        for group_id in group_ids:
-            try:
-                message = random.choice(messages)
-                await client.send_message(int(group_id), message)
-                await asyncio.sleep(2)
-                await client.send_message(5488677608, f" Sent message to group {group_id}")
-            except Exception as e:
-                await client.send_message(5488677608, f"Error sending message to group {group_id}: {e}")
-                
-                
-@client.on(events.NewMessage(outgoing=True, pattern='!cancel'))
+        for channel_id in channel_ids:
+            message = random.choice(messages)
+            await client.send_message(channel_id, message)
+        await asyncio.sleep(DELAY)  # Send a message every 30 minutes
+
+@client.on(events.NewMessage(outgoing=True, pattern='!ccancel'))
 async def handle_cancel(event):
     await event.respond('Cancelling Auto Message Forwarding...')
     global send_task
     send_task.cancel()
 
-@client.on(events.NewMessage(outgoing=True, pattern='!start'))
+@client.on(events.NewMessage(outgoing=True, pattern='!cstart'))
 async def handle_start(event):
     await event.respond("Starting Auto Message Forwarding...")
     global send_task
     send_task = asyncio.create_task(send_messages())
 
-logging.basicConfig(level=logging.WARNING)
-
 with client:
     client.run_until_disconnected()
+        
