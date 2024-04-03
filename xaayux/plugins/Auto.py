c6aef4d54008e34d523f193627925c75cbfc94bc
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-04-03 23:30:51 +0530

    Update Auto.py

--- xaayux/plugins/Auto.py
+++ xaayux/plugins/Auto.py
@@ -12,7 +12,7 @@ async def send_messages():
             button1 = types.Button.url("Premium Folder 🥳", "https://t.me/addlist/-uKaqPRmwAU5NmM1")
             button2 = types.Button.url("Add 👆", "https://t.me/xaayux")
             await client.send_message(channel_id, message, buttons=[button1, button2])
-        await asyncio.sleep(DELAY)  # Send a message every 30 minutes
+            await asyncio.sleep(DELAY)  # Send a message every 30 minutes
         
 @client.on(events.NewMessage(outgoing=True, pattern='!ccancel'))
 async def handle_cancel(event):
