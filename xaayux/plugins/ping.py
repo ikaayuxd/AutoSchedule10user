346feb7430fb2fa30b066c1219086fae7ea18cdd
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-04-07 08:49:34 +0530

    Update ping.py

--- xaayux/plugins/ping.py
+++ xaayux/plugins/ping.py
@@ -50,7 +50,10 @@ async def fwdrmv(event):
         else:
             await event.client.send_message(event.chat_id, event.message)
             await event.delete()
-
+        except Exception as e:
+        # Handle exceptions here
+         print(f"An error occurred: {e}")
+         
 
 
   
