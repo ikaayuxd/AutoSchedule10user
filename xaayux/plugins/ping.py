41abbdb2659617fe5d2f8e691a31ca4f34f47f0e
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-09-10 18:12:29 +0530

    Update ping.py

--- xaayux/plugins/ping.py
+++ xaayux/plugins/ping.py
@@ -1,21 +1,10 @@
 import re
 from .. import client
-from telethon import events
-from telethon.tl import types 
-import logging 
-import asyncio
-import time
-from telethon.tl.functions.messages import EditMessageRequest
-from xaayux.config import DELAY, channel_ids 
 import asyncio
 from telethon import TelegramClient, events, types
 from telethon.tl.functions.messages import SendReactionRequest
+from telethon.tl.types import Reaction
 
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-# -- Constants -- #
 @client.on(events.NewMessage)
 async def reaction_handler(event):
     chat = await event.get_chat()
@@ -23,7 +12,7 @@ async def reaction_handler(event):
     await client(SendReactionRequest(
         peer=chat,
         msg_id=message_id,
-        reaction=[types.ReactionEmoji(
-            emoticon='❤️'
-        )]
+        reaction=Reaction(
+            emoticon='❤' 
+        )
     ))
