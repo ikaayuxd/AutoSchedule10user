4da64b4a0db82f3f4238814bc169b53c212eb483
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-09-17 11:23:46 +0530

    Update ping.py

--- xaayux/plugins/ping.py
+++ xaayux/plugins/ping.py
@@ -1,15 +1,22 @@
 import re
-from .. import client # Import both clients
+from .. import client, client2# Import both clients
 from telethon import TelegramClient, events, functions, types
 import asyncio
 
 # Define your channel IDs as a single variable
 CHANNEL_IDS = [-1001966404031, -1002495106403] # Replace with your actual channel IDs
 @client.on(events.NewMessage(chats=CHANNEL_IDS))
+@client2.on(events.NewMessage(chats=CHANNEL_IDS))
 async def handler_client1(event):
     if event.is_channel:
         try:
             await event.client(functions.messages.SendReactionRequest(
+                peer=event.chat_id,
+                msg_id=event.id,
+                big=True,
+                add_to_recent=True,
+                reaction=[types.ReactionEmoji(emoticon='❤️')]
+            await event.client2(functions.messages.SendReactionRequest(
                 peer=event.chat_id,
                 msg_id=event.id,
                 big=True,
