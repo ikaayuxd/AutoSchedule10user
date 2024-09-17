6c67e3ca8d8c9ec2e427e5f560690eced06d89e4
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-09-17 10:22:45 +0530

    Update ping.py

--- xaayux/plugins/ping.py
+++ xaayux/plugins/ping.py
@@ -1,5 +1,5 @@
 import re
-from .. import client
+from .. import client, client2
 from telethon import TelegramClient, events, functions, types
 import asyncio
 
@@ -20,6 +20,27 @@ async def handler(event):
                     reaction=[types.ReactionEmoji(emoticon='❤️')]
                 ))
                 print(f"Reacted to message from {event.chat.title} with ❤️")
+
+                await asyncio.sleep(2) # Flood wait of 2 seconds before the next reaction
+
+            except Exception as e:
+                print(f"Error: {e}")
+
+
+@client2.on(events.NewMessage)
+async def handler(event):
+    if event.is_channel: # Check if the message is from a channel
+        # Check if the channel ID is in the list
+        if event.chat_id in CHANNEL_IDS:
+            try:
+                await client2(functions.messages.SendReactionRequest(
+                    peer=event.chat_id,
+                    msg_id=event.id,
+                    big=True,
+                    add_to_recent=True,
+                    reaction=[types.ReactionEmoji(emoticon='💀')]
+                ))
+                print(f"Reacted to message from {event.chat.title} with 💀")
 
                 await asyncio.sleep(2) # Flood wait of 2 seconds before the next reaction
 
