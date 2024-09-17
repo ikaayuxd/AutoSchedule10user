5382ecb1e9ea0b7473fef406fa65cbe344cd67f1
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-09-17 10:17:23 +0530

    Update __init__.py

--- xaayux/__init__.py
+++ xaayux/__init__.py
@@ -3,26 +3,39 @@ from os import environ
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
-from xaayux.config import API_ID, API_HASH, SESSION, channel_ids, DELAY
+from xaayux.config import API_ID, API_HASH, SESSION, SESSION2, channel_ids, DELAY
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
-
+# Session 1
 if SESSION is not None:
-    session = StringSession(str(SESSION))
+    session1 = StringSession(str(SESSION))
 else:
-    session = "pyrobot"
+    session1 = "pyrobot"
+
+# Session 2
+if SESSION2 is not None:
+    session2 = StringSession(str(SESSION2))
+else:
+    session2 = "pyrobot"
 
 try:
     client = TelegramClient(
-        session=session,
+        session=session1,
         api_id=API_ID,
         api_hash=API_HASH,
         connection=ConnectionTcpAbridged
     )
     client.start()
-    
+
+    client2 = TelegramClient(
+        session=session2,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        connection=ConnectionTcpAbridged
+    )
+    client2.start()
+
 except Exception as e:
     print(e)
-
