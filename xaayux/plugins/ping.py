9b13e3f1869f4a84176c8ebd4fb34099c0802718
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-09-17 10:40:17 +0530

    Update ping.py

--- xaayux/plugins/ping.py
+++ xaayux/plugins/ping.py
@@ -7,6 +7,7 @@ import asyncio
 CHANNEL_IDS = [-1001966404031, -1002495106403] # Replace with your actual channel IDs
 
 @client.on(events.NewMessage)
+@client2.on(events.NewMessage)
 async def handler(event):
     if event.is_channel: # Check if the message is from a channel
         # Check if the channel ID is in the list
@@ -20,27 +21,6 @@ async def handler(event):
                     reaction=[types.ReactionEmoji(emoticon='❤️')]
                 ))
                 print(f"Reacted to message from {event.chat.title} with ❤️")
-
-                await asyncio.sleep(2) # Flood wait of 2 seconds before the next reaction
-
-            except Exception as e:
-                print(f"Error: {e}")
-
-
-@client2.on(events.NewMessage)
-async def handler2(event):
-    if event.is_channel: # Check if the message is from a channel
-        # Check if the channel ID is in the list
-        if event.chat_id in CHANNEL_IDS:
-            try:
-                await client2(functions.messages.SendReactionRequest(
-                    peer=event.chat_id,
-                    msg_id=event.id,
-                    big=True,
-                    add_to_recent=True,
-                    reaction=[types.ReactionEmoji(emoticon='🥰')]
-                ))
-                print(f"Reacted to message from {event.chat.title} with 🥰")
 
                 await asyncio.sleep(2) # Flood wait of 2 seconds before the next reaction
 
