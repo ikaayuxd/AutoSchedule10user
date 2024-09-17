b24002e36f790f3a0978d20ff78de0f07b982c5e
Jhovkvj <117099782+ikaayuxd@users.noreply.github.com>
2024-09-17 11:17:39 +0530

    Update ping.py

--- xaayux/plugins/ping.py
+++ xaayux/plugins/ping.py
@@ -1,11 +1,10 @@
 import re
-from .. import client, client2 # Import both clients
+from .. import client # Import both clients
 from telethon import TelegramClient, events, functions, types
 import asyncio
 
 # Define your channel IDs as a single variable
 CHANNEL_IDS = [-1001966404031, -1002495106403] # Replace with your actual channel IDs
-
 @client.on(events.NewMessage(chats=CHANNEL_IDS))
 async def handler_client1(event):
     if event.is_channel:
@@ -21,23 +20,3 @@ async def handler_client1(event):
             await asyncio.sleep(2)
         except Exception as e:
             print(f"Client 1 Error: {e}")
-
-@client2.on(events.NewMessage(chats=CHANNEL_IDS))
-async def handler_client2(event):
-    if event.is_channel:
-        try:
-            await event.client(functions.messages.SendReactionRequest(
-                peer=event.chat_id,
-                msg_id=event.id,
-                big=True,
-                add_to_recent=True,
-                reaction=[types.ReactionEmoji(emoticon='🥰')]
-            ))
-            print(f"Client 2 reacted to message from {event.chat.title} with 🥰")
-            await asyncio.sleep(2)
-        except Exception as e:
-            print(f"Client 2 Error: {e}")
-
-with client2:
-    client2.run_until_disconnected()
-        
